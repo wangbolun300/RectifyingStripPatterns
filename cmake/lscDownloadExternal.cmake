37ed7d0d603b21f0ccff2670a7ffc5700b0d9613
Bolun Wang <wangbolun@buaa.edu.cn>
2025-06-30 11:11:50 +0200

    Update lscDownloadExternal.cmake

--- cmake/lscDownloadExternal.cmake
+++ cmake/lscDownloadExternal.cmake
@@ -1,51 +1,90 @@
+################################################################################
 include(DownloadProject)
 
 # With CMake 3.8 and above, we can hide warnings about git being in a
 # detached head by passing an extra GIT_CONFIG option
 if(NOT (${CMAKE_VERSION} VERSION_LESS "3.8.0"))
-    set(SPARSE_INTERP_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
+    set(FAST_ENVELOPE_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
 else()
-    set(SPARSE_INTERP_EXTRA_OPTIONS "")
+    set(FAST_ENVELOPE_EXTRA_OPTIONS "")
 endif()
 
-function(sparse_interp_download_project name)
+# Shortcut function
+function(fast_envelope_download_project name)
     download_project(
         PROJ         ${name}
-        SOURCE_DIR   ${SPARSE_EXTERNAL}/${name}
-        DOWNLOAD_DIR ${SPARSE_EXTERNAL}/.cache/${name}
+        SOURCE_DIR   ${FAST_ENVELOPE_EXTERNAL}/${name}
+        DOWNLOAD_DIR ${FAST_ENVELOPE_EXTERNAL}/.cache/${name}
         QUIET
-        ${SPARSE_INTERP_EXTRA_OPTIONS}
+        ${FAST_ENVELOPE_EXTRA_OPTIONS}
         ${ARGN}
     )
 endfunction()
 
 ################################################################################
 
+## libigl
+function(fast_envelope_download_libigl)
+    if(TARGET igl::core)
+    return()
+    endif()
 
-# Eigen
-function(sparse_interp_download_eigen)
-  sparse_interp_download_project(eigen
-  GIT_REPOSITORY           https://gitlab.com/libeigen/eigen.git
-	GIT_TAG       3.4.0
-  )
+    include(FetchContent)
+    FetchContent_Declare(
+    libigl
+    GIT_REPOSITORY https://github.com/libigl/libigl.git
+    GIT_TAG v2.4.0
+    )
+    FetchContent_MakeAvailable(libigl)
 endfunction()
 
 
-
-# libigl for timing and mesh processing
-function(sparse_interp_download_libigl)
-   sparse_interp_download_project(libigl
-   GIT_REPOSITORY https://github.com/libigl/libigl.git
-   GIT_TAG        aea868bd1fc64f71afecd2c51e51507a99d8e3e5
-  )
+## CLI11
+function(fast_envelope_download_cli11)
+    fast_envelope_download_project(cli11
+        URL     https://github.com/CLIUtils/CLI11/archive/v1.6.1.tar.gz
+        URL_MD5 48ef97262adb0b47a2f0a7edbda6e2aa
+    )
 endfunction()
 
-# A modern string formatting library
-function(sparse_interp_download_fmt)
-  sparse_interp_download_project(fmt
-    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG        6.2.0
-  )
+## tbb
+# function(fast_envelope_download_tbb)
+#     fast_envelope_download_project(tbb
+#         GIT_REPOSITORY https://github.com/uxlfoundation/oneTBB.git
+#         GIT_TAG        8acdf22f9ea88feef3c85e520abab683fb2734d7
+#     )
+# endfunction()
+
+
+## Sanitizers
+function(fast_envelope_download_sanitizers)
+    fast_envelope_download_project(sanitizers-cmake
+        GIT_REPOSITORY https://github.com/arsenm/sanitizers-cmake.git
+        GIT_TAG        6947cff3a9c9305eb9c16135dd81da3feb4bf87f
+    )
+endfunction()
+
+## spdlog
+function(fast_envelope_download_spdlog)
+    fast_envelope_download_project(spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG        188cff7d6567b80c6b99bc15899fef9637a8fe52
+    )
+endfunction()
+
+## Geogram LGPL
+function(fast_envelope_download_geogram)
+    fast_envelope_download_project(geogram
+        GIT_REPOSITORY  https://github.com/polyfem/geogram.git
+        GIT_TAG        516c151c244d9019a9076a1a468d52a0f6dd195d
+    )
 endfunction()
 
 
+# ## Geogram predicates LGPL
+# function(fast_envelope_download_geogram_predicates)
+#     fast_envelope_download_project(geogram_predicates
+#         URL     https://gforge.inria.fr/frs/download.php/file/38199/Predicates_psm_1.7.2.tar.gz
+#         URL_MD5 4635a4e19538514ee2d6183af791e280
+#     )
+# endfunction()
